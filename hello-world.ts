90783b9d76dd0ea954f2d13bcb2aa3ea52a076b2
briha <briha@test.com>
2020-08-02 01:54:26 +0530

    TypeScript Adde

---
+++ hello-world.ts
@@ -0,0 +1,7 @@
+class Author{
+    constructor (public name: string) {}
+}
+
+let author = new Author("Brihadeesh R K");
+console.log("Hello World!");
+console.log("This is +${author.Name}+s Repository");
