74938a8f25be6980d2964ba1625eea3917dca355
Brihadeesh <brihadeeshrk@gmail.com>
2020-08-02 08:14:56 +0530

    SQL Added

---
+++ hello-world.sql
@@ -0,0 +1,6 @@
+CREATE TABLE helloworld (phrase TEXT);
+.tables
+CREATE TABLE helloworld (phrase TEXT);
+INSERT INTO helloworld VALUES ("Hello, World!");
+INSERT INTO helloworld VALUES ("Goodbye, World!");
+SELECT COUNT(*) FROM helloworld;
