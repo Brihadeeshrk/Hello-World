92dbad22ed1e02ce158382f68f423022ed7b1223
briha <briha@test.com>
2020-08-02 08:20:43 +0530

    C# Added

---
+++ hello-world/hello-world/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Brihadeesh";
+            var repositoryDetails = "GitHub: Hello World Repository"
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
