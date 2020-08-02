9ee5820c962b38f1a15b815e4bb8898880d82c04
briha <briha@test.com>
2020-08-02 08:28:04 +0530

    Swift added

---
+++ hello-world/hello-world/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hello-world
+//
+//  Created by briha on 02/08/20.
+//  Copyright © 2020 briha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
